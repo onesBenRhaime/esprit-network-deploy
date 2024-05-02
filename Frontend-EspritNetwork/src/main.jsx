2578936db0fb8425e321fbda9569184ae78879fc
onesBenRhaime <onesrhaime28@gmail.com>
2024-05-02 15:45:21 +0100

    v-deploy

--- Frontend-EspritNetwork/src/main.jsx
+++ Frontend-EspritNetwork/src/main.jsx
@@ -1,8 +1,8 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
-
 import { Provider } from "react-redux";
 import store from "./Redux/store.js";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<Provider store={store}>
 		<App />
