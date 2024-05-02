7fde214f3fb827f40e590176c501d8c0aab0263b
onesBenRhaime <onesrhaime28@gmail.com>
2024-05-02 15:57:33 +0100

    v-deploy

--- Frontend-EspritNetwork/vite.config.js
+++ Frontend-EspritNetwork/vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			// Map React import to Preact
+			react: "preact/compat",
+			"react-dom/test-utils": "preact/test-utils",
+			"react-dom": "preact/compat",
+		},
+	},
 });
