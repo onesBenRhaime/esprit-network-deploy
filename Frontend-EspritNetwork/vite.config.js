0928c681b8ae045aaca5ee562f1f0465a3c60ae4
onesBenRhaime <onesrhaime28@gmail.com>
2024-05-02 04:22:13 +0100

    v0-deploy

--- Frontend-EspritNetwork/vite.config.js
+++ Frontend-EspritNetwork/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			external: [
+				"assets/vendor/aos/aos.js",
+				"assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+				"assets/vendor/glightbox/js/glightbox.min.js",
+				"assets/vendor/isotope-layout/isotope.pkgd.min.js",
+				"assets/vendor/swiper/swiper-bundle.min.js",
+				"assets/vendor/waypoints/noframework.waypoints.js",
+				"assets/vendor/php-email-form/validate.js",
+				"assets/js/main.js",
+				"https://kit.fontawesome.com/867167fad8.js",
+				"https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/themes/df-messenger-default.css",
+			],
+		},
+	},
+});
